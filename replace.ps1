7a84f0d7fda035af815908432adf8d794d285d26
GeReV <amirgrozki@gmail.com>
2026-03-25 07:24:26 +0200

    Added a global search and replace script.

---
+++ replace.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+    [Parameter(Mandatory)]
+    [string] $From,
+
+    [Parameter(Mandatory)]
+    [string] $To
+)
+
+Get-ChildItem -Recurse .\files\ | Where-Object { $_.Extension -in ".srt", ".json" } | ForEach-Object {
+    Write-Output $_
+
+    if (Select-String -Path $_ -Pattern $From)
+    {
+        (Get-Content $_) -replace $From, $To |
+            Set-Content $_
+    }
+}
