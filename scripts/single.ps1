abf4503d98b705b71f877a249b8343874135300b
Amir Grozki <amirgrozki@gmail.com>
2026-03-28 23:21:55 +0300

    Updated single.ps1 script.

--- scripts/single.ps1
+++ scripts/single.ps1
@@ -22,8 +22,9 @@ $outfile = Join-Path $outdir $file.BaseName
 $outscenes = Join-Path $outdir "$($file.BaseName).scenes"
 $outlog = Join-Path $outdir "$($file.BaseName).log"
 
+# The Hebrew-refined models are available from: https://huggingface.co/ivrit-ai
 $cmd = "ffmpeg -nostdin -threads 0 -i `"`$file`"` -f wav -ac 1 -acodec pcm_s16le -ar 16000 - |
-	$WhisperExe -m ..\whisper.cpp\models\ggml-large-v3-turbo.bin -vm ..\whisper.cpp\models\ggml-silero-v6.2.0.bin --vad -l he -ojf -osrt -of temp -pp -et 2.8 -mc 64 -f - 2>&1 |
+	$WhisperExe -m ..\whisper.cpp\models\ivrit-ggml-large-v3-turbo.bin -vm ..\whisper.cpp\models\ggml-silero-v6.2.0.bin --vad -l he -ojf -osrt -of temp -pp -et 2.8 -mc 64 -f - 2>&1 |
 	Tee-Object -FilePath `"`$outlog`"` "
 	
 Invoke-Expression $cmd
